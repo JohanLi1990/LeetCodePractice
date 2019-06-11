d67d79d440c843fb4333f19f477aea3158a95243
LI CHENYANG <li.chenyang1990@gmail.com>
2019-06-11 22:28:26 +0800

    BubbleSort Revision
    Redo some bubble sort code, corrected some false association

--- SortPractice/src/BubbleSort.java
+++ SortPractice/src/BubbleSort.java
@@ -3,19 +3,20 @@ import java.util.Arrays;
 public class BubbleSort implements ISortTechnique {
 
     @Override
-    public int[] sort(int[] sourceArray){
+    public int[] sort(int[] sourceArray) {
         int[] arr = Arrays.copyOf(sourceArray, sourceArray.length);
-        for (int i = 1; i < arr.length; i++) {
-            boolean flag = true;
-            for (int j = 0; j < arr.length - 1; j++) {
+        for (int i = 1; i < sourceArray.length; i++) {
+            boolean needToSwitch = false;
+            for (int j = 0; j < sourceArray.length - i; j++) {
                 if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j + 1];
-                    arr[j + 1] = arr[j];
-                    arr[j] = temp;
-                    flag = false;
+                    //switch
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    needToSwitch = true;
                 }
             }
-            if (flag){
+            if(!needToSwitch){
                 break;
             }
         }
--- SortPractice/src/MainSort.java
+++ SortPractice/src/MainSort.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class MainSort {
     public static void main(String[] args) {
         System.out.println("Sort Example:.....\n");
-        int[] input = new int[]{1, 3, 4, 2, 10, 8};
+        int[] input = new int[]{1, 3, 4, 5, 2, 10, 8, -1};
         BubbleSort bs = new BubbleSort();
         System.out.println(bs.getName() + ": " + Arrays.toString(bs.sort(input)));
     }
