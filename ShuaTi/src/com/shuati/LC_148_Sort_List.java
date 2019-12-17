87f1089f76d7dd1f90a96976828b35613f1df3a1
LI CHENYANG <li.chenyang1990@gmail.com>
2019-12-17 23:12:55 +0800

    LC_148
    get the idea first

---
+++ ShuaTi/src/com/shuati/LC_148_Sort_List.java
@@ -0,0 +1,16 @@
+package com.shuati;
+
+public class LC_148_Sort_List {
+    public ListNode sortList(ListNode head) {
+        //Sort a linked list in O(n log n) time using constant space complexity.
+        //O(1) O(n Log n)
+        //two pointer, fast and slow
+        //split two lines
+        //then merge together
+    }
+
+    private ListNode mergeSort(ListNode head, int first, int second) {
+
+
+    }
+}
