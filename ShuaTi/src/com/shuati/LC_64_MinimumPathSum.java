9561d7fffd37791597d675d6af7bad69ded508c9
LI CHENYANG <li.chenyang1990@gmail.com>
2019-12-22 23:22:24 +0800

    LC64
    Optimized

---
+++ ShuaTi/src/com/shuati/LC_64_MinimumPathSum.java
@@ -0,0 +1,21 @@
+package com.shuati;
+
+public class LC_64_MinimumPathSum {
+    public int minPathSum(int[][] grid) {
+        for (int i = 0; i < grid.length ; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                //case 1
+                if (i == 0 && j == 0){
+                    //do nothing
+                } else if (i == 0){
+                    grid[i][j] += grid[i][j - 1];
+                } else if (j == 0){
+                    grid[i][j] += grid[i - 1][j];
+                } else {
+                    grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+                }
+            }
+        }
+        return grid[grid.length - 1][grid[0].length - 1];
+    }
+}
