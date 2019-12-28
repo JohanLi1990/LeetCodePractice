253aa30567f2fa6955c892d92ab04c3972adcccd
LI CHENYANG <li.chenyang1990@gmail.com>
2019-12-28 23:31:37 +0800

    LC_121 optimized
    dynamic programming in one dim

--- ShuaTi/src/com/shuati/LC_121_Best_Time_Buy_Sell_Stock.java
+++ ShuaTi/src/com/shuati/LC_121_Best_Time_Buy_Sell_Stock.java
@@ -2,23 +2,15 @@ package com.shuati;
 
 public class LC_121_Best_Time_Buy_Sell_Stock {
     public int maxProfit(int[] prices) {
-
-        if (prices.length < 2){
-            return 0;
-        }
-
+        int min = Integer.MAX_VALUE;
         int max = 0;
-        for (int i = 0; i < prices.length; i++) {
-            if (max > prices[i]){
+        for(int price : prices){
+            if (max > price){
                 continue;
             }
-
-            for (int j = 0; j < i ; j++) {
-                max = Math.max(max, prices[i] - prices[j]);
-            }
-
+            min = Math.min(min, price);
+            max = Math.max(max, price - min);
         }
         return max;
-
     }
 }
