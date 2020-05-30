ac467903b55b105c8e5e5910ab7a772726dbdeee
LI CHENYANG <li.chenyang1990@gmail.com>
2020-05-30 13:52:22 +0800

    Delete from source

--- ShuaTi/src/com/shuati/ListNode.java
+++
@@ -1,8 +0,0 @@
-package com.shuati;
-public class ListNode {
-    public int val;
-    public ListNode next;
-    public ListNode(int x){
-        val = x;
-    }
-}
--- SortPractice/src/ISortTechnique.java
+++
@@ -1,4 +0,0 @@
-public interface ISortTechnique {
-    public int[] sort(int[] sourceArray);
-    public String getName();
-}
